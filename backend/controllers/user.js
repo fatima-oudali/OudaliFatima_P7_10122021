6d3b70bf259ec33e7d1a3b5af7cdc11289e48039
fatima <oud.tima@hotmail.com>
2021-12-15 15:06:03 +0100

    user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -29,6 +29,27 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
     const email = req.body.email;
     db.query('SELECT * FROM user WHERE email =?', [email], (err, result) => {
-        
+        User.findOne({email: req.body.email})
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({error: 'Utilisateur non trouvé !'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if (!valid){
+                return res.status(401).json({error: 'Mot de passe incorrect !'})
+            }
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign (
+                    {userId: user._id},
+                    `${process.env.JWT_SECRET}`,
+                    {expiresIn: '24h'}
+                )
+            })
+        })
+        .catch(error => res.status(500).json({error}));
+    })
+    .catch(error => res.status(500).json({error}));
     })
 };
