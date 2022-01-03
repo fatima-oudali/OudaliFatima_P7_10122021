2b0d42331dd5ec597941251646bb1ef95ef1a0e0
fatima <oud.tima@hotmail.com>
2022-01-03 13:59:45 +0100

    maj

--- frontend/src/components/Log/SignInForm.js
+++ frontend/src/components/Log/SignInForm.js
@@ -1,3 +1,69 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+
+// const SignInForm = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+//     const emailError = document.querySelector(".email.error");
+//     const passwordError = document.querySelector(".password.error");
+
+//     axios({
+//       method: "post",
+//       url: `${process.env.REACT_APP_API_URL}api/auth/login`,
+//       withCredentials: true,
+//       data: {
+//         email,
+//         password,
+//       },
+//     })
+//       .then((res) => {
+//         console.log(res);
+//         if (res.data.error) {
+//           emailError.innerHTML = res.data.error.email;
+//           passwordError.innerHTML = res.data.error.password;
+//         } else {
+//           window.location = "/";
+//         }
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+
+//   return (
+//     <form action="" onSubmit={handleLogin} id="sign-up-form">
+//       <label htmlFor="email">Email</label>
+//       <br />
+//       <input
+//         type="text"
+//         name="email"
+//         id="email"
+//         onChange={(e) => setEmail(e.target.value)}
+//         value={email}
+//       />
+//       <div className="email error"></div>
+//       <br />
+//       <label htmlFor="password">Mot de passe</label>
+//       <br />
+//       <input
+//         type="password"
+//         name="password"
+//         id="password"
+//         onChange={(e) => setPassword(e.target.value)}
+//         value={password}
+//       />
+//       <div className="password error"></div>
+//       <br />
+//       <input type="submit" value="Se connecter" />
+//     </form>
+//   );
+// };
+
+// export default SignInForm;
+
 import React, { useState } from "react";
 import axios from "axios";
 
@@ -20,7 +86,7 @@ const SignInForm = () => {
       },
     })
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
         if (res.data.error) {
           emailError.innerHTML = res.data.error.email;
           passwordError.innerHTML = res.data.error.password;
