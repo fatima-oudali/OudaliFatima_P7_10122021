3772ceab4599acb4728b0083c6f208eb0ba0fc37
fatima <oud.tima@hotmail.com>
2022-02-21 02:09:02 +0100

    export sql

--- groupomania .sql
+++ groupomania .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 13 jan. 2022 à 22:33
+-- Généré le : lun. 21 fév. 2022 à 02:07
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.0.13
 
@@ -31,8 +31,38 @@ CREATE TABLE `comment` (
   `id` int(11) NOT NULL,
   `contenu` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `post_id` int(11) NOT NULL
+  `post_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `comment`
+--
+
+INSERT INTO `comment` (`id`, `contenu`, `user_id`, `post_id`, `date`) VALUES
+(2, 'comment çava ?', 21, 5, '2022-02-17 21:11:39'),
+(3, 'Slt ', 28, 4, '2022-02-17 21:11:39'),
+(5, 'Hi !', 20, 6, '2022-02-17 21:11:39'),
+(27, 'Bonjour', 21, 5, '2022-02-19 17:18:22'),
+(28, 'Bonjour', 21, 5, '2022-02-19 17:18:26'),
+(29, 'comment çava', 21, 5, '2022-02-19 17:19:20'),
+(30, 'salut', 21, 5, '2022-02-21 00:48:12'),
+(31, 'cc', 21, 4, '2022-02-21 00:49:39'),
+(32, 'salutt', 21, 4, '2022-02-21 00:51:24'),
+(33, 'salutt', 21, 9, '2022-02-21 00:51:42'),
+(34, 'hello', 21, 4, '2022-02-21 00:56:02'),
+(35, 'Hi', 21, 4, '2022-02-21 00:57:05'),
+(36, 'slt', 21, 9, '2022-02-21 00:57:29'),
+(37, 'slt', 21, 9, '2022-02-21 00:57:32'),
+(38, 'slt', 21, 9, '2022-02-21 00:57:33'),
+(39, 'slt', 21, 9, '2022-02-21 00:57:35'),
+(40, 'slt', 21, 9, '2022-02-21 00:57:36'),
+(41, 'slt', 21, 9, '2022-02-21 00:57:36'),
+(42, 'slt', 21, 9, '2022-02-21 00:57:38'),
+(43, 'slt', 21, 9, '2022-02-21 00:57:38'),
+(44, 'slt', 21, 9, '2022-02-21 00:57:48'),
+(45, '...', 21, 5, '2022-02-21 01:02:05'),
+(46, 'cc', 21, 6, '2022-02-21 01:05:01');
 
 -- --------------------------------------------------------
 
@@ -47,6 +77,16 @@ CREATE TABLE `post` (
   `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `post`
+--
+
+INSERT INTO `post` (`id`, `contenu`, `user_id`, `date`) VALUES
+(4, 'Salut', 29, '2022-02-02 21:41:00'),
+(5, 'coucou cava ', 23, '2022-02-16 23:38:29'),
+(6, 'Hello ^_^', 21, '2022-02-17 15:55:32'),
+(9, 'Il fait beau aujourd\'hui !', 21, '2022-02-19 17:51:44');
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +95,7 @@ CREATE TABLE `post` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -63,12 +104,13 @@ CREATE TABLE `user` (
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `password`) VALUES
-(1, 'test@test.com', '$2b$10$zuvzHmv.mVDWBZUdevZiIe60qsKUfFjggYi85.hGEQJzTs0aerera'),
-(2, '', '$2b$10$hQ.UOwixS7kQPcVWj0iLV.4FWio95ZLOjY4lQwwYChLN.b7UNNc0C'),
-(4, 't@test.com', '$2b$10$4YFgdShhwjaBXc46ztjTa.zrVDUuOdpwDbGqA8vxZwfevgZUaM1Oq'),
-(6, 't2@test.com', '$2b$10$Tqz74E8bAZveRnQXV3QF/uVLEW6CtjU/1GeqmjJDCVkEYovxKyg6a'),
-(7, 'oud.tima@hotmail.com', '$2b$10$Fyqhkm0wQ9ijnv0JL/L/neZIzX/5MvIRCy5oDnDZBOIUSgv3zEaBK');
+INSERT INTO `user` (`id`, `pseudo`, `email`, `password`) VALUES
+(20, 'f', 'f@test.com', '$2b$10$yIcYXTUGE7pA2HbNt4cyA.AUPY2pW8buyRjxbhFsUKJHXbtLsQ6Wi'),
+(21, 'fatima', 'oud.tima@hotmail.com', '$2b$10$NgdnXX7CQEqZttAl3DxMg.WSd3A2C18QLgzPSXfcbde6fYbxSWpOm'),
+(23, 'test1', 'test1@test.com', '$2b$10$WbZEXwWDm4oE5pPzI7LVBewLIwNsADAuR8FYLqQhSc45b1vkUyDWG'),
+(26, 'test1', 'test11@test.com', '$2b$10$Y3IkwcIm4bP6L3FFYvpZIOYVR584VTFqjk0QHEWohQr1nWG3McnsW'),
+(28, 'timazar', 't@test.com', '$2b$10$5I4Ru4LQV3gIyW0GA.fCeuNYeijmMdu2AAIDxm.UqUjoVPeb60noO'),
+(29, 'zora', 'zora@test.com', '$2b$10$7/wPYia9KGGUoLnsEa2qw.oHd1UidzyDNYZAPcx.h8Ms3yM7C07FG');
 
 --
 -- Index pour les tables déchargées
@@ -104,19 +146,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Contraintes pour les tables déchargées
